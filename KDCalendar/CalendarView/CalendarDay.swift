5c14e122fb89f67302aa26364fb30dbada905ad9
Vit Chernuhin <vit.chernuhin@icloud.com>
2023-10-27 00:02:13 +0700

    Added CalendarDay

---
+++ KDCalendar/CalendarView/CalendarDay.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarDay.swift
+//  CalendarView
+//
+//  Created by Vit Chernuhin on 26.10.2023.
+//  Copyright © 2023 Karmadust. All rights reserved.
+//
+
+import Foundation
+
+struct CalendarDay {
+    let date: Date
+    let isActive: Bool
+}
